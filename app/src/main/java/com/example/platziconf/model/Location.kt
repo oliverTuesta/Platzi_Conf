0917c588bb99f42184d2cc4f1cb7c1c4bbfc6f3e
OliverTuesta <oliver.jtuesta@gmail.com>
2020-10-27 21:10:07 -0500

    coneccion con la base de datos fireBase y model creados

---
+++ app/src/main/java/com/example/platziconf/model/Location.kt
@@ -0,0 +1,12 @@
+package com.example.platziconf.model
+
+class Location {
+
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val longitude = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
---
+++ app/src/main/java/com/example/platziconf/model/Speaker.kt
@@ -0,0 +1,12 @@
+package com.example.platziconf.model
+
+class Speaker{
+
+    var name = ""
+    var jobTitle = ""
+    var workPlace = ""
+    var biography = ""
+    var twitter = ""
+    var picture = ""
+
+}
